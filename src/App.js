9e6cdfa6eea4c6af7e25acfbf5d0469da94a1272
Tech-Sikshya <90173813+Tech-Sikshya@users.noreply.github.com>
2022-08-26 00:15:08 +0545

    nees

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <center>
+        <h3>This is pandey sudip here.</h3>
+      </center>
+    </div>
+  );
 }
 
 export default App;
